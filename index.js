d544e6cf77fbc544e6e8b6a1e40e97b706f89454
srikar <srikar.code01@gmail.com>
2025-01-14 20:33:32 +0530

    installed and added dotenv package

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const { log } = require('console');
 const express = require('express');
+require('dotenv').config()
 
 const app = express();
 
@@ -6,4 +8,6 @@ app.get('/', function(req, res) {
     res.send('Hello World!')
 })
 
-app.listen(3000);
+// YOU HAVE TO CHANGE THE VALU FROM .env FILE AS WE GENERALLY GER STRING FROM THE FILE. SO I HAVE PARSED IT TO INT FORMAT
+app.listen(parseInt(process.env.PORT, 10));
+// app.listen(process.env.PORT) // this wont work because the port is in the string format not as a number
