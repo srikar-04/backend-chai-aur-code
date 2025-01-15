6ec1d407d579fbbd8b59b75df7430fb485f2b1c0
srikar <srikar.code01@gmail.com>
2025-01-15 10:08:11 +0530

    updated PORT variable, added check to it

--- 01_basic-fullstack-application/backend/index.js
+++ 01_basic-fullstack-application/backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 
-const PORT = parseInt(process.env.PORT, 10)
+const PORT = parseInt(process.env.PORT, 10) || 5000
 
 app.get('/', (req, res) => {
     res.send('hello')
