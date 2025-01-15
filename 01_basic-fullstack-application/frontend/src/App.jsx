eaad39914081b66d98195246f90250c72b3e4364
srikar <srikar.code01@gmail.com>
2025-01-15 11:09:21 +0530

    frontend state setup and axios setup

--- 01_basic-fullstack-application/frontend/src/App.jsx
+++ 01_basic-fullstack-application/frontend/src/App.jsx
@@ -1,33 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [jokes, setJokes] = useState([])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Basic fullstack setup</h1>
+      <h1>NO OF JOKES : {jokes.length}</h1>
+
+      {jokes.map(joke => {
+        <div key={joke.id}>
+          <h1>{joke.title}</h1>
+          <p>{joke.content}</p>
+        </div>
+      })}
     </>
   )
 }
