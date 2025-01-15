2ccbd56c7044ed870712990ff8d3373bd477de3d
srikar <srikar.code01@gmail.com>
2025-01-15 10:51:05 +0530

    added another route to server

--- 01_basic-fullstack-application/backend/index.js
+++ 01_basic-fullstack-application/backend/index.js
@@ -7,10 +7,36 @@ import 'dotenv/config'
 const app = express();
 
 
-const PORT = parseInt(process.env.PORT, 10) || 5000
+const PORT = parseInt (process.env.PORT, 10) || 5000
 
 app.get('/', (req, res) => {
     res.send('hello')
+})
+
+app.get('/jokes', (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: 'joke 1',
+            content: 'this is a joke',
+        },
+        {
+            id: 2,
+            title: 'joke 2',
+            content: 'this is a joke',
+        },
+        {
+            id: 3,
+            title: 'joke 3',
+            content: 'this is a joke',
+        },
+        {
+            id: 4,
+            title: 'joke 4',
+            content: 'this is a joke',
+        }
+    ]
+    res.send(jokes)
 })
 
 
